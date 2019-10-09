c51deeca946d90b3b3e478bcd6a7d350e2ceb67c
szykol <szymonkolton@gmail.com>
2019-10-09 22:34:35 +0200

    Create exception hierarchy

---
+++ src/Exception.h
@@ -0,0 +1,15 @@
+#pragma once 
+
+#include <stdexcept>
+
+class PotatoException : public std::runtime_error
+{
+public:
+    PotatoException(const std::string& error = "Potato Base Exception") : std::runtime_error(error) {} 
+};
+
+class BadRequest : public PotatoException
+{
+public:
+    BadRequest() : PotatoException("Bad HTTP Request") {}
+};
