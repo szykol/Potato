56816963d17607e346bca2f72c1d798d25223c80
szykol <szymonkolton@gmail.com>
2019-10-28 22:33:52 +0100

    Add python end to end tests

---
+++ test/EndToEnd/EndToEndTest.py
@@ -0,0 +1,26 @@
+import subprocess
+import requests
+import time
+import unittest
+import multiprocessing
+import json
+
+class EndToEndTest(unittest.TestCase):
+    def setUp(self):
+        self.sb = subprocess.Popen('build/potato')
+
+    def tearDown(self):
+        self.sb.kill()
+
+    def test_potato_works(self):
+        """ Test if potato responds correctly """
+        r = requests.get('http://localhost:8080')
+        self.assertEqual(r.status_code, 200)
+
+    def test_method_not_allowed(self):
+        r = requests.get('http://localhost:8080/say_hi')
+        self.assertEqual(r.status_code, 200)
+        r = requests.post('http://localhost:8080/say_hi', data=json.dumps({'msg': 'hi'}))
+        self.assertEqual(r.status_code, 405)
+        self.assertEqual(r.text, 'Method Not Allowed')
+        
