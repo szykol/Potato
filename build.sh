016e3fbd1d12a499bb251655479e05440a11344e
szykol <szymonkolton@gmail.com>
2019-11-01 00:30:29 +0100

    Add submodule init and cmake check

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-mkdir -p build
-cd build
-cmake ..
-make
+if [ ! "$(ls -A googletest)" ]; then
+    echo "googletest submodule does not exist"
+    echo "initializing submodules"
+    git submodule update --init --recursive
+fi
+
+if hash cmake 2>/dev/null; then
+    mkdir -p build
+    cd build
+    cmake ..
+    make
+else
+    echo "cmake is not installed. Please try again with cmake installed"
+fi
