c7462e3c50e3fb83ca73c53ef9ecb523e3238c7e
szykol <szymonkolton@gmail.com>
2019-11-05 23:55:27 +0100

    Add flag that enables logging

--- src/Log.h
+++ src/Log.h
@@ -13,9 +13,17 @@ public:
 };
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#define LOG_ERROR(fmt, ...) ::Log::GetLogger()->error(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
-#define LOG_INFO(fmt, ...) ::Log::GetLogger()->info(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
-#define LOG_WARN(fmt, ...) ::Log::GetLogger()->warn(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
-#define LOG_TRACE(fmt, ...) ::Log::GetLogger()->trace(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
-#define LOG_DEBUG(fmt, ...) ::Log::GetLogger()->debug(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
 
+#ifdef POTATO_DEBUG
+    #define LOG_ERROR(fmt, ...) ::Log::GetLogger()->error(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
+    #define LOG_INFO(fmt, ...) ::Log::GetLogger()->info(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
+    #define LOG_WARN(fmt, ...) ::Log::GetLogger()->warn(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
+    #define LOG_TRACE(fmt, ...) ::Log::GetLogger()->trace(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
+    #define LOG_DEBUG(fmt, ...) ::Log::GetLogger()->debug(fmt " [{}:{}]", ##__VA_ARGS__, __FILENAME__, __LINE__)
+#else
+    #define LOG_ERROR(fmt, ...) 
+    #define LOG_INFO(fmt, ...) 
+    #define LOG_WARN(fmt, ...) 
+    #define LOG_TRACE(fmt, ...) 
+    #define LOG_DEBUG(fmt, ...) 
+#endif
