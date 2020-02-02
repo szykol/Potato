dc8f6c86f768f4b2287d40409a6cfe8c123ee20d
szykol <szymonkolton@gmail.com>
2020-02-02 19:29:41 +0100

    Add more folders to gitignore

--- debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /var/users/szykol/potato
-# Build directory: /var/users/szykol/potato/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("spdlog")
-subdirs("test")
